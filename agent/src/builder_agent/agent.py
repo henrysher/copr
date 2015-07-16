272e0d47ad6190664b233f694100629b2ceb685f
Valentin Gologuzov <vgologuz@redhat.com>
2015-07-16 13:19:58 +0200

    [agent] now it's look good; running process until it's finish while reading it's stdout/stderr, respect timeout and able to cancel running process.

--- agent/src/builder_agent/agent.py
+++ agent/src/builder_agent/agent.py
@@ -1,17 +1,64 @@
 # coding: utf-8
 from shlex import split
 
+import os
 import sys
 import time
 import asyncio
-from asyncio import coroutine, Lock, async
+from asyncio import coroutine, Lock, async, create_subprocess_exec
 from aiohttp import web
 from logging import getLogger
 from backports.typing import List
 
 log = getLogger(__name__)
 
-running_build_lock = Lock()
+BUFFER_SIZE = 4096
+
+
+class BuildContext(object):
+
+    def __init__(self):
+        self.started_on = None
+        self.ended_on = None
+        self.cancelled_on = None
+
+        self.timeout_exceeded = False
+
+        self.stdout = bytearray()
+        self.stderr = bytearray()
+        self.return_code = None
+
+        self.stdout_done_future = asyncio.Future()
+        self.stderr_done_future = asyncio.Future()
+
+    @property
+    def status(self):
+        if self.started_on is None:
+            return "ready"
+        elif self.cancelled_on is not None:
+            return "cancelled"
+        elif self.ended_on is not None:
+            return "finished"
+        else:
+            return "running"
+
+    def __str__(self):
+        text = self.status + "\n"
+        if self.timeout_exceeded:
+            text += "WARNING: timeout exceeded\n"
+        if self.return_code:
+            text += "return_code: {}\n".format(self.return_code)
+        try:
+            text += "stdout length: {}:\n".format(len(self.stdout)) + bytes(self.stdout).decode("utf-8")
+        except Exception:
+            log.exception("failed add stdout to output")
+
+        try:
+            text += "stderr length: {}:\n".format(len(self.stderr)) + bytes(self.stderr).decode("utf-8")
+        except Exception:
+            log.exception("failed add stderr to output")
+
+        return text
 
 
 class Daemon(object):
@@ -22,168 +69,159 @@ class Daemon(object):
     """
     def __init__(self, build_cmd: str, timeout: int):
         log.info("Daemon initialized")
+        log.info("CMD to exec:\n{}".format(build_cmd))
         self.loop = asyncio.get_event_loop()
 
-        self.build_cmd = build_cmd
+        self.cmd = build_cmd
         self.timeout = timeout or 3600 * 6
 
-        self.build_started_on = None
-        self.build_ended_on = None
-        self.build_stdout = bytearray()
-        self.build_stderr = bytearray()
-        self.build_return_code = None
+        self.proc = None
+        self.build = BuildContext()
 
-        self.timeout_reached_at = None
         self.build_is_running = False
         self.finalize_invoked = False
 
-        self.kill_switch_future = None
+        self.kill_switch_task = None
+        self.stdout_read_task = None
+        self.stderr_read_task = None
+
+    @coroutine
+    def kill_switch(self):
+        log.info("started kill switch")
+        yield from asyncio.sleep(self.timeout)
+        log.info("kill switch activated")
+        if self.build.status == "running":
+            self.build.timeout_exceeded = True
+            asyncio.async(self.finalize())
 
     @coroutine
     def start_watched_build(self):
         self.build_is_running = True
 
-        create = asyncio.create_subprocess_exec(
-            *split(self.build_cmd),
-            limit=127,
+        self.proc = yield from create_subprocess_exec(
+            *split(self.cmd),
+            env=os.environ.copy(),  # or we just need to set PYTHONUBUFFERED=true
+            # limit=127,
             stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE)
+            stderr=asyncio.subprocess.PIPE
+        )
 
-        self.build_proc = yield from create
-        self.build_started_on = time.time()
-
+        self.build.started_on = time.time()
         async(self.wait_for_build())
-        # since 4.3.3
-        # self.kill_switch_future = asyncio.ensure_future(self.kill_switch, loop=self.loop)
-        self.kill_switch_future = asyncio.async(self.kill_switch(), loop=self.loop)
-
-    def cancel_ks_safe(self):
-        log.info("trying to  cancel KS")
-        if self.kill_switch_future:
-            self.kill_switch_future.cancel()
-            log.info("canceled KS")
+        self.kill_switch_task = async(self.kill_switch())
+        # todo: after python 4.3.3 user asyncio.ensure_future
 
     @coroutine
-    def kill_switch(self):
-        log.info("KS start sleep")
-        yield from asyncio.sleep(self.timeout)
-        log.info("KS activated -- timeout occurred")
-        yield from self.finalize()
+    def read_stream(self, stream: asyncio.StreamReader, target, on_done_future: asyncio.Future, name=None):
+        name = name or "stream"
+        while self.proc and self.proc.returncode is None and not self.finalize_invoked:
+            if stream.at_eof():
+                break
+            log.info("trying to read from {}".format(name))
+            b_data = yield from stream.read(BUFFER_SIZE)
+            if b_data:
+                target.extend(b_data)
+                log.info("read {} bytes from {}".format(len(b_data), name))
+
+            if len(b_data) == BUFFER_SIZE:
+                yield from asyncio.sleep(0.0001)
+            elif len(b_data) > 0:
+                yield from asyncio.sleep(0.02)
+            else:
+                yield from asyncio.sleep(0.1)
+        if not on_done_future.done():
+            log.info("set future {} done".format(name))
+            on_done_future.set_result(True)
 
     @coroutine
     def wait_for_build(self):
-        log.info("Before wait for")
-        # try:
-        # we have a problem here - if coroutine is cancelled, underlying `yield from proc.communicate()`
-        #   would block stdout.read(), so we implement external timeout which calls self.finalize
-        # yield from asyncio.wait_for(self.read_result(), self.timeout)
-        yield from self.read_result()
-        # except asyncio.TimeoutError:
-        #     log.exception("On timeout error")
-        #     self.build_ended_on = self.timeout_reached_at = time.time()
-        #     yield from self.read_rest()
+        log.debug("Wait for result")
 
-        log.info("After wait for")
-        # import ipdb; ipdb.set_trace()
-        if not self.finalize_invoked:
-            yield from self.finalize()
+        self.stdout_read_task = asyncio.async(self.read_stream(
+            self.proc.stdout, self.build.stdout, self.build.stdout_done_future, "stdout"))
+        self.stderr_read_task = asyncio.async(self.read_stream(
+            self.proc.stderr, self.build.stderr, self.build.stderr_done_future, "stderr"))
 
-    # @coroutine
-    # def read_rest(self):
-    #     log.info("try to read rest")
-    #     tmp_stdout = yield from self.build_proc.stdout.read()
-    #     log.info("tmp stdout: {}".format(tmp_stdout))
-
-    @coroutine
-    def read_result(self):
-        log.info("wait for result")
-        if self.build_proc and not self.build_proc.stdout.at_eof():
-            self.build_stdout, self.build_stderr = yield from self.build_proc.communicate()
-        log.info("yielded from communicate")
-        self.cancel_ks_safe()
-
-        # could be continued after timeout & terminate invoked
-        # if not self.build_return_code and self.build_proc:
-        #     self.build_return_code = self.build_proc.returncode
-
-        log.info("Build finished: {}".format(self.build_return_code))
-        self.build_ended_on = time.time()
+        try:
+            yield from self.build.stdout_done_future
+            yield from self.build.stderr_done_future
+        except asyncio.CancelledError:
+            log.info("got cancelled error, it's fine")
+        except Exception as err:
+            log.exception(err)
+        log.info("Yielded stdout/stderr")
+        yield from self.finalize()
 
     @coroutine
     def finalize(self):
-        self.cancel_ks_safe()
+        if self.finalize_invoked:
+            log.info("Called finalize twice or more")
+            return
 
         self.finalize_invoked = True
-        log.info("Finalize invoked")
-        print(repr(self.build_proc))
+        log.info("Finalize invoked for child: '{}'".format(self.proc))
+        for task in [self.kill_switch_task, self.stderr_read_task, self.stdout_read_task]:
+            if task:
+                task.cancel()
 
+        for read_future in [self.build.stdout_done_future, self.build.stderr_done_future]:
+            if not read_future.done():
+                log.info("cancelling future")
+                read_future.cancel()
 
         try:
-            self.build_proc.terminate()
-            self.build_return_code = yield from self.build_proc.wait()
+            self.proc.terminate()
+        except ProcessLookupError:
+            pass # usually means that process is already halt
+        except Exception as err:
+            log.exception("err terminate")
+
+        try:
+            self.build.return_code = yield from self.proc.wait()
         except Exception:
             log.exception("err waiting")
 
         log.info("Finalize invoked - wait done")
-        self.build_proc = None
+        self.proc = None
+
+        log.info("Build finished: {}".format(self.build.return_code))
+        self.build.ended_on = time.time()
+
         self.build_is_running = False
 
     @coroutine
     def cancel_build(self, request):
         log.info("< cancel")
-        if not self.build_proc:
+        if not self.proc:
             return web.Response(body=b"No running build to cancel", status=400)
         else:
+            self.build.cancelled_on = time.time()
             yield from self.finalize()
-            return web.Response(body=b"No running build to cancel", status=400)
+            return web.Response(body=b"Cancelling build", status=200)
 
     @coroutine
     def start_build(self, request):
-        log.info("< start")
+
         if self.build_is_running:
+            log.info("< start REJECTED")
             return web.Response(body=b"Build is running\n", status=400)
 
+        log.info("< start EXECUTING")
         self.reset_results()
 
-        with (yield from running_build_lock):
-            yield from self.start_watched_build()
-            log.info("Build started")
-            text = "Build started\n"
-            return web.Response(body=text.encode('utf-8'))
-
-    # @coroutine
-    def status(self, request):
-        log.info("< status")
-        if self.build_started_on is None:
-            text = "No build started yet\n"
-        elif self.build_ended_on is not None:
-            text = "Build finished in {} seconds\n".format(self.build_ended_on - self.build_started_on)
-        else:
-            text = "Time elapsed: {}, latest build was started at: {}\n"\
-                .format(time.time() - self.build_started_on, self.build_started_on)
-
-        try:
-            text += "stdout: {}\n".format(len(self.build_stdout)) + bytes(self.build_stdout).decode("utf-8")
-        except Exception:
-            log.exception("failed add stdout to output")
-
-        try:
-            text += "stderr:\n" + bytes(self.build_stderr).decode("utf-8")
-        except Exception:
-            log.exception("failed add stderr to output")
-
-        # import ipdb; ipdb.set_trace()
-        # if self.build_protocol:
-        #     text += "output: {}".format(self.build_protocol.stdout)
-
-        # if self.build_return_code:
-        #     text += "return code: {}".format(self.build_return_code)
-        # if self.build_stdout:
-        #     text += "stdout: {}".format(self.build_stdout)
-        # if self.build_stderr:
-        #     text += "stderr: {}".format(self.build_stderr)
-
+        # this app should be single threaded, at least the loop with http handler
+        # so we don't need to use Lock to start new build
+        yield from self.start_watched_build()
+        log.info("Build started")
+        text = "Build started\n"
         return web.Response(body=text.encode('utf-8'))
+
+    def status(self, request):
+        """
+        For debug purpose
+        """
+        log.info("< status")
+        return web.Response(body=str(self.build).encode('utf-8'))
 
     @coroutine
     def init(self):
@@ -196,25 +234,23 @@ class Daemon(object):
         print("Server started at http://127.0.0.1:8080")
         return srv
 
-    def attach_error_handler(self):
-
-        def mute_err_on_terminate(loop, context):
-            if 'Exception in callback SubprocessStreamProtocol.process_exited()' == context.get("message"):
-                # see: https://bugs.python.org/issue23140
-                log.info("muted err")
-            else:
-                loop.default_exception_handler(context)
-
-        self.loop.set_exception_handler(mute_err_on_terminate)
+    # def attach_error_handler(self):
+    #     def mute_err_on_terminate(loop, context):
+    #         if 'Exception in callback SubprocessStreamProtocol.process_exited()' == context.get("message"):
+    #             # see: https://bugs.python.org/issue23140
+    #             log.info("muted err")
+    #         else:
+    #             loop.default_exception_handler(context)
+    #
+    #     self.loop.set_exception_handler(mute_err_on_terminate)
 
     def run(self):
-        self.attach_error_handler()
+        # self.attach_error_handler()
         self.loop.run_until_complete(self.init())
         self.loop.run_forever()
 
     def reset_results(self):
-        self.build_started_on = None
-        self.build_stdout = bytearray()
-        self.build_stderr = bytearray()
-        self.build_return_code = None
+        self.build = BuildContext()
+
+        self.build_is_running = False
         self.finalize_invoked = False
--- agent/src/runner.py
+++ agent/src/runner.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         datefmt='%H:%M:%S',
     )
     build_cmd_mock = (
-        "/usr/bin/mockchain -r fedora-21-x86_64 -l /var/tmp/mockremote-x5/build/ "
+        "/usr/bin/mockchain -r fedora-21-x86_64 -l /var/tmp/mockremote-x32xssx2/build/ "
         "-a https://copr-be.cloud.fedoraproject.org/results/vgologuz/test_copr/fedora-21-x86_64/ "
         "-a https://copr-be.cloud.fedoraproject.org/results/vgologuz/test_copr/fedora-21-x86_64/devel/ "
         "-m '--define=copr_username vgologuz' -m '--define=copr_projectname test_copr' "
@@ -18,12 +18,16 @@ if __name__ == "__main__":
         "http://miroslav.suchy.cz/copr/copr-ping-1-1.fc20.src.rpm")
 
     yes_cmd = "yes"
-
+    sleep_cmd = "sleep 3"
+    df_cmd = "lsof"
     d = Daemon(
-        # build_cmd="wget http://miroslav.suchy.cz/copr/copr-ping-1-1.fc20.src.rpm",
-        build_cmd=yes_cmd
+        # build_cmd="wget http://miroslav.suchy.cz/copr/copr-ping-1-1.fc20.src.rpm"
+        # build_cmd="wget https://download.fedoraproject.org/pub/fedora/linux/releases/22/Workstation/x86_64/iso/Fedora-Live-Workstation-x86_64-22-3.iso"
+        # build_cmd=yes_cmd
         # build_cmd=build_cmd_mock
-        , timeout=10
+        build_cmd=sleep_cmd
+        # build_cmd=df_cmd
+        , timeout=2
     )
     d.run()
 
